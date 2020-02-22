54e856416e72c37a360c31cc4877ae13b6278207
Matthew Yu <matthewjkyu@gmail.com>
2020-02-21 19:54:50 -0600

    test_encoders.py

--- drivers/encoders/test_encoders.py
+++ drivers/encoders/test_encoders.py
@@ -15,4 +15,7 @@ e.setup()
 input = input("Press enter to quit\n\n")
 e.read(0) # should return an error message
 e.read(1) # should return 0 since motor isn't moving.
+e.reset(0) # should return an error message
+e.reset(1) # should work. Read should be 0 now if it wasn't already.
+e.read(1)
 e.shutdown()
